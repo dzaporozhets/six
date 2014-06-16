915cc7c9fafe87267b1739374eae5d71b97b5b81
Darren Cauthon <darren@cauthon.com>
2014-06-16 17:24:55 -0500

    Strip the tests.

--- spec/spec_helper.rb
+++
@@ -1,4 +0,0 @@
-require "rubygems"
-require "bundler"
-Bundler.require(:default, :development)
-Dir[File.dirname(__FILE__) + '/support/*.rb'].each {|file| require file }
--- spec/support/author.rb
+++
@@ -1,7 +0,0 @@
-class Author
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
