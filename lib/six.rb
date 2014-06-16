fdfcda5e57f7bc605daf3f9aab3a1f7b26b154cf
Darren Cauthon <darren@cauthon.com>
2014-06-16 18:53:05 -0500

    Still cutting things down.

--- lib/six.rb
+++ lib/six.rb
@@ -17,10 +17,10 @@ class Six
     @rules
   end
 
-  def action_included?(object, action, subject)
+  def action_included? object, action, subject
     permissions = rules.map do |rp| 
                               begin
-                                rp.allowed(object, subject)
+                                rp.allowed object, subject
                               rescue
                                 []
                               end
@@ -28,13 +28,12 @@ class Six
 
     rejection_rules = []
 
-    rules.map do |rule_pack|
-      next unless rule_pack.respond_to?(:prevented)
+    rules.select { |r| r.respond_to? :prevented }.map do |rule_pack|
       rejection_rules << rule_pack.prevented(object, subject)
     end
     rejection_rules = rejection_rules.flatten.map { |x| x.to_s } 
 
-    permissions.reject! { |x| rejection_rules.include?(x.to_s) }
+    permissions.reject! { |x| rejection_rules.include? x.to_s }
     permissions.include? action.to_s
   end
 
