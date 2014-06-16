daca5e104116b12dd26a33774809d5c9785471b8
Darren Cauthon <darren@cauthon.com>
2014-06-16 17:38:50 -0500

    Add a smoke test to verify the basic functionality.

--- spec/six_spec.rb
+++ spec/six_spec.rb
@@ -4,4 +4,25 @@ describe Six do
   it "should exist" do
     true.must_equal true
   end
+
+  describe "smoke tests" do
+
+    it "should return the results from the rules class" do
+
+      expected_results = [:apple]
+      subject          = Object.new
+      target           = Object.new
+
+      rule_set = Object.new
+      rule_set.stubs(:allowed).with(subject, target).returns expected_results
+
+      abilities = Six.new
+      abilities << rule_set
+
+      abilities.allowed?(subject, :apple, target).must_equal true
+      abilities.allowed?(subject, :orange, target).must_equal false
+
+    end
+  end
+
 end
