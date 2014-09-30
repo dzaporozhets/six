d53a2e087f7dc208b2bac52687d4b5031f79a4c4
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-09-30 17:36:16 +0300

    Add coveralls to spec helper
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require "rubygems"
 require "bundler"
 Bundler.require(:default, :development)
+require 'coveralls'
+Coveralls.wear!
+
 Dir[File.dirname(__FILE__) + '/support/*.rb'].each {|file| require file }
