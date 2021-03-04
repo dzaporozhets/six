a9a20ba2deb1c0a1212e966a28d5e6220521cc6e
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2021-03-04 18:43:47 +0200

    Fix some indentaion in spec
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- spec/support/valid_abilities_example.rb
+++ spec/support/valid_abilities_example.rb
@@ -1,11 +1,11 @@
 shared_examples :valid_abilities do
   describe :allowed? do
     before do
-      @jim = Author.new("Jim")
-      @mike = Author.new("Mike")
+      @jim = Author.new('Jim')
+      @mike = Author.new('Mike')
 
-      @jims_book = Book.new("The Game", @jim)
-      @mikes_book = Book.new("Life", @mike)
+      @jims_book = Book.new('The Game', @jim)
+      @mikes_book = Book.new('Life', @mike)
     end
 
     def allowed?(action, object, subject)
@@ -17,33 +17,33 @@ shared_examples :valid_abilities do
         abilities.use(rules_key).allowed?(action, object, subject)
     end
 
-    describe "should return true or false depend on access" do
+    describe 'should return true or false depend on access' do
       context :read_book do
-        it { allowed?(@jim,  :read_book, @jims_book).should be_truthy }
+        it { allowed?(@jim, :read_book, @jims_book).should be_truthy }
+        it { allowed?(@jim, :read_book, @mikes_book).should be_truthy }
         it { allowed?(@mike, :read_book, @mikes_book).should be_truthy }
-        it { allowed?(@jim,  :read_book, @mikes_book).should be_truthy }
         it { allowed?(@mike, :read_book, @jims_book).should be_truthy }
       end
 
       context :rate_book do
-        it { allowed?(@jim,  :rate_book, @jims_book).should be_falsey }
+        it { allowed?(@jim, :rate_book, @jims_book).should be_falsey }
+        it { allowed?(@jim, :rate_book, @mikes_book).should be_truthy }
         it { allowed?(@mike, :rate_book, @mikes_book).should be_falsey }
-        it { allowed?(@jim,  :rate_book, @mikes_book).should be_truthy }
         it { allowed?(@mike, :rate_book, @jims_book).should be_truthy }
       end
 
       context :edit_book do
         it { allowed?(@jim, :edit_book, @jims_book).should be_truthy }
-        it { allowed?(@mike,:edit_book,  @mikes_book).should be_truthy }
         it { allowed?(@jim, :edit_book, @mikes_book).should be_falsey }
-        it { allowed?(@mike,:edit_book,  @jims_book).should be_falsey }
+        it { allowed?(@mike, :edit_book, @mikes_book).should be_truthy }
+        it { allowed?(@mike, :edit_book, @jims_book).should be_falsey }
       end
 
       context :publish_book do
         it { allowed?(@jim, :publish_book, @jims_book).should be_falsey }
-        it { allowed?(@mike,:publish_book,  @mikes_book).should be_falsey }
         it { allowed?(@jim, :publish_book, @mikes_book).should be_falsey }
-        it { allowed?(@mike,:publish_book,  @jims_book).should be_falsey }
+        it { allowed?(@mike, :publish_book, @mikes_book).should be_falsey }
+        it { allowed?(@mike, :publish_book, @jims_book).should be_falsey }
       end
 
       context 'passing multiple actions' do
